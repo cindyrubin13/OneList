7b223e4b21e44271f5c07598497eb9070c488770
Cindy Rubin <cindyrubin13@gmail.com>
2013-11-05 18:33:00 -0500

    itiial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OneList::Application.config.secret_key_base = '4a529649bb9f863c9037d03d047bf984afffc0ea81f33a77d0a5db4dfa182fd13d267a2d1987a82fc56a75d6a645d48a780b4d3e5d373b365e3b33e2ef18eaaf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OneList::Application.config.session_store :cookie_store, key: '_OneList_session'
